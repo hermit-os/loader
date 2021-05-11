5a7648231ca5c7c74748e40b87c49da8250bf306
Jonathan Klimt <jonathan.klimt@eonerc.rwth-aachen.de>
2021-05-11 20:33:16 +0200

    slighly improved error message for missing cli utils

--- build.rs
+++ build.rs
@@ -14,12 +14,12 @@ fn main() {
 			.args(&["src/arch/x86_64/entry.asm", "-felf64", "-o"])
 			.arg(&format!("{}/entry.o", out_dir))
 			.status()
-			.unwrap();
+			.expect("Could not start nasm. Is it installed?");
 		Command::new("ar")
 			.args(&["crus", "libentry.a", "entry.o"])
 			.current_dir(&Path::new(&out_dir))
 			.status()
-			.unwrap();
+			.expect("Could not start ar. Is it installed?");
 
 		println!("cargo:rustc-link-search=native={}", out_dir);
 		println!("cargo:rustc-link-lib=static=entry");
