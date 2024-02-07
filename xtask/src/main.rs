6be1c675f8c47300cd8a331317482ee920c6b500
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2024-02-07 17:12:06 +0100

    fix(xtask): centralize feature flags
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -34,21 +34,12 @@ impl flags::Build {
 
 		eprintln!("Building loader");
 		let triple = self.target.triple();
-		if self.target == Target::X86_64Fc {
-			cmd!(sh, "cargo build --target={triple}")
-				.env("CARGO_ENCODED_RUSTFLAGS", self.cargo_encoded_rustflags()?)
-				.args(self.target_dir_args())
-				.args(self.profile_args())
-				.args(&["--no-default-features"])
-				.args(&["--features", "fc"])
-				.run()?;
-		} else {
-			cmd!(sh, "cargo build --target={triple}")
-				.env("CARGO_ENCODED_RUSTFLAGS", self.cargo_encoded_rustflags()?)
-				.args(self.target_dir_args())
-				.args(self.profile_args())
-				.run()?;
-		}
+		cmd!(sh, "cargo build --target={triple}")
+			.env("CARGO_ENCODED_RUSTFLAGS", self.cargo_encoded_rustflags()?)
+			.args(self.target.feature_flags())
+			.args(self.target_dir_args())
+			.args(self.profile_args())
+			.run()?;
 
 		let build_object = self.build_object();
 		let dist_object = self.dist_object();
