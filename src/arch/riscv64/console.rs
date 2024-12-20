8eff2ec087eba4d3a968533fd32761e45c1fb046
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2024-12-20 17:32:53 +0100

    fix(riscv64): use correct serial buffer pointer provenance

--- src/arch/riscv64/console.rs
+++ src/arch/riscv64/console.rs
@@ -6,6 +6,6 @@ pub struct Console(());
 
 impl Console {
 	pub fn write_bytes(&mut self, bytes: &[u8]) {
-		sbi_rt::console_write(Physical::new(bytes.len(), bytes.as_ptr().addr(), 0));
+		sbi_rt::console_write(Physical::new(bytes.len(), bytes.as_ptr().expose_addr(), 0));
 	}
 }
