ae6983219b4501b152398fc44573bdf783f097fa
Fritz Stracke <fritz.stracke@rwth-aachen.de>
2025-07-31 14:36:22 +0200

    boot independently of current exception level
    when booting, this commit adds a check to determine the machines current
    exception level. If it is lower than 1, it changes the EL accordingly.
    
    Signed-off-by: Fritz Stracke <fritz.stracke@rwth-aachen.de>

--- src/arch/aarch64/entry.s
+++ src/arch/aarch64/entry.s
@@ -31,8 +31,57 @@ _start:
 	adrp	x4, __boot_core_stack_end_exclusive
 	add		x4, x4, #:lo12:__boot_core_stack_end_exclusive
 	mov		sp, x4
-	
+
+	// Set correct Exception level!
+
+	mrs	x0, CurrentEL
+	cmp	x0, #4  // EL = 1
+	b.eq	el_1_entry
+
+	// Test if EL2
+	cmp	x0, #8
+	b.eq	el_2_entry
+
+	//EL3
+	msr	SP_EL2, x4
+	msr	SP_EL1, x4
+	msr	SCTLR_EL2, xzr
+	msr	HCR_EL2, xzr
+
+	mrs	x0, SCR_EL3
+	and	x0, x0, #(~(1 << 3))
+	and	x0, x0, #(~(1 << 2))
+	and	x0, x0, #(~(1 << 1))
+	orr	x0, x0, #(1<<10)
+	orr	x0, x0, #(1<<0)
+	msr	SCR_EL3, x0
+
+	mov	x0, #0b1111001001 // D-Flag, I-FLAG, A-FLAG, F-FLAG, EL2h
+	msr	SPSR_EL3, x0
+
+	adr	x0, el_2_entry
+	msr	ELR_EL3, x0
+
+	eret
+
+// EL2
+el_2_entry:
+	msr	SP_EL1, x4
+	msr	SCTLR_EL1, xzr
+	mrs	x0, HCR_EL2
+	orr	x0, x0, #(1<<31)
+	msr	HCR_EL2, x0
+
+	mov	x0, #0b1111000101 // D-Flag, I-FLAG, A-FLAG, F-FLAG, EL1h
+	msr	SPSR_EL2, x0
+
+	adr	x0, el_1_entry
+	msr	ELR_EL2, x0
+
+	eret
+
 	// Jump to Rust code.
+el_1_entry:
 	b	{start_rust}
 
 	// Infinitely wait for events (aka "park the core").
